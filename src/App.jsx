462291235975ada2ad23dcac088faa730be4cd8a
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-06-14 16:38:53 -0700

    Implemented vawe_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import chatMessages from './data/messages.json';
+
+
 
 const App = () => {
+  const firstMessage = chatMessages[1];
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {/* Wave 01: Render one ChatEntry component*/}
+        <ChatEntry
+          sender={firstMessage.sender}
+          body={firstMessage.body}
+          timeStamp={firstMessage.timeStamp}
+        />
+        {/*Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
